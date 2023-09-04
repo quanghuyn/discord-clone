e7678f9bb42f1374e4d91cf37a718c42836d9b74
quanghuyn <huybo106@gmai.com>
2023-09-04 15:21:25 +0700

    init

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+const AuthLayou = ({children}:{children: React.ReactNode}) => {
+    return ( 
+    <div className="h-full flex items-center justify-center " >
+        {children}
+    </div> );
+}
+ 
+export default AuthLayou;
